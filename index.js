c81dbc33f5b419e96fb6a38f711257ef56a4f22a
OxidiLily <oxidilily@gmail.com>
2024-09-21 00:46:45 +0700

    update

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const mineflayer = require('mineflayer')
 const config = require('./settings.json');
 
 const bot = mineflayer.createBot({
-    host: 'localhost', // minecraft server ip
+    host: "OxidiLilyServer332.aternos.me", // minecraft server ip
     username: 'Bot', // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
-    auth: 'microsoft' // for offline mode servers, you can set this to 'offline'
-        // port: 25565,              // set if you need a port that isn't 25565
-        // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
-        // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
+    auth: 'mojang', // for offline mode servers, you can set this to 'offline'
+    port: 34971, // set if you need a port that isn't 25565
+    // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+    // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
 })
 
 // Log errors and kick reasons:
